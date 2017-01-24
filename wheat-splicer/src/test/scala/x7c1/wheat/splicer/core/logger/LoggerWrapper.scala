c92b65b86037344c01bdf7bf7592f6d1e6c52d39
haco <t.aida@japantaxi.co.jp>
2017-01-24 23:56:59 +0900

    rename

--- wheat-splicer/src/test/scala/x7c1/wheat/splicer/core/logger/LoggerWrapper.scala
+++ wheat-splicer/src/test/scala/x7c1/wheat/splicer/core/logger/LoggerWrapper.scala
@@ -1,10 +1,10 @@
-package x7c1.wheat.splicer.core
+package x7c1.wheat.splicer.core.logger
 
-import org.slf4j.Logger
+import org.slf4j
 import sbt.ProcessLogger
 
 
-case class LoggerWrapper(logger: Logger) extends ProcessLogger {
+case class LoggerWrapper(logger: slf4j.Logger) extends ProcessLogger {
 
   override def info(s: => String): Unit = {
     logger.info(s)
