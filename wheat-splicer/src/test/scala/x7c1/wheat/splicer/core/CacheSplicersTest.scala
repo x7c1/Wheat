8054c43888449fdf6499241ef8222c13b7bcf7ed
x7c1 <jmtaro+x7c1@gmail.com>
2017-01-28 21:27:27 +0900

    add test for .sourceDirectories

--- wheat-splicer/src/test/scala/x7c1/wheat/splicer/core/CacheSplicersTest.scala
+++ wheat-splicer/src/test/scala/x7c1/wheat/splicer/core/CacheSplicersTest.scala
@@ -1,37 +1,51 @@
 package x7c1.wheat.splicer.core
 
-import org.scalatest.{FlatSpecLike, Matchers}
-import sbt.{file, richFile}
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+import sbt.{file, globFilter, richFile, singleFileFinder}
 import x7c1.wheat.splicer.android.AndroidSdk
 import x7c1.wheat.splicer.android.PropertyLoader.{buildToolsVersion, compileSdkVersion, dependencies, sdkRoot}
 import x7c1.wheat.splicer.core.logger.Logging
 
-class CacheSplicersTest extends FlatSpecLike with Matchers with Logging {
+class CacheSplicersTest extends FlatSpecLike
+  with Matchers with BeforeAndAfterAll with Logging with SplicerSettings {
 
-  behavior of CacheSplicers.getClass.getSimpleName
+  override protected def beforeAll(): Unit = {
+    splicers.cleanAll run logger
+    splicers.expandAll run logger
+  }
 
-  it can "expand aar files" in {
+  behavior of "expandAll method"
 
-    logger info "hello..."
-
-    val project = file("./sample-splicer-client")
-    val sdk = AndroidSdk(
-      sdkRoot = sdkRoot via (project / "local.properties"),
-      buildToolsVersion = buildToolsVersion fromResource "/build.gradle",
-      compileSdkVersion = compileSdkVersion fromResource "/build.gradle"
+  ".sourceDirectories" must "contain generated R.java files" in {
+    val expected = Seq(
+      unmanaged / "appcompat-v7/src-generated/android/support/v7/appcompat/R.java",
+      unmanaged / "cardview-v7/src-generated/android/support/v7/cardview/R.java",
+      unmanaged / "design/src-generated/android/support/design/R.java",
+      unmanaged / "recyclerview-v7/src-generated/android/support/v7/recyclerview/R.java"
     )
-    val factory = new CacheSplicers.Factory(
-      sdk = sdk,
-      unmanagedDirectory = project / "libs-expanded"
+    val actualPaths = splicers.sourceDirectories.map(_ ** "R.java").flatMap(_.getPaths)
+    expected.map(_.getPath) foreach { path =>
+      actualPaths should contain(path)
+    }
+  }
+
     )
     val splicers = factory create (dependencies fromResource "/target.gradle")
 
     println(splicers.sourceDirectories)
 
-    //    splicers.cleanAll run logger
-    //    splicers.expandAll run logger
+}
 
-    true shouldBe true
+trait SplicerSettings {
+  val project = file("./sample-splicer-client")
+  val sdk = AndroidSdk(
+    sdkRoot = sdkRoot via (project / "local.properties"),
+    buildToolsVersion = buildToolsVersion fromResource "/build.gradle",
+    compileSdkVersion = compileSdkVersion fromResource "/build.gradle"
+  )
+  val unmanaged = (project / "libs-expanded").getCanonicalFile
+  val splicers = {
+    val factory = new CacheSplicers.Factory(unmanaged, sdk)
+    factory create (dependencies fromResource "/target.gradle")
   }
-
 }
