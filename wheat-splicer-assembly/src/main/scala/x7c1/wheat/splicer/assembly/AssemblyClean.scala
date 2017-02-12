6325db49107c586c1436a5bf253eed69dc02dd31
x7c1 <jmtaro+x7c1@gmail.com>
2017-02-12 23:52:58 +0900

    remove needless Initialize

--- wheat-splicer-assembly/src/main/scala/x7c1/wheat/splicer/assembly/AssemblyClean.scala
+++ wheat-splicer-assembly/src/main/scala/x7c1/wheat/splicer/assembly/AssemblyClean.scala
@@ -1,8 +1,7 @@
 package x7c1.wheat.splicer.assembly
 
-import sbt.Def
 import sbt.Def.{SettingList, SettingsDefinition}
-import sbt.Keys.clean
+import sbt.Keys.{clean, streams}
 import sbtassembly.AssemblyKeys.{assembly, assemblyOutputPath}
 import x7c1.wheat.splicer.assembly.AssemblySettingKeys.splicerAssemblyClean
 import x7c1.wheat.splicer.lib.FileCleaner
@@ -11,11 +10,11 @@ import x7c1.wheat.splicer.lib.FileCleaner
 object AssemblyClean {
 
   def settings: SettingsDefinition = new SettingList(Seq(
-    splicerAssemblyClean := assemblyClean.value,
+    splicerAssemblyClean := {
+      val path = (assemblyOutputPath in assembly).value
+      FileCleaner.withLogging remove path run streams.value.log
+    },
     clean := clean.dependsOn(splicerAssemblyClean).value
   ))
 
-  private val assemblyClean = Def taskDyn {
-    FileCleaner.withLogging remove (assemblyOutputPath in assembly).value
-  }
 }
--- wheat-splicer/src/main/scala/x7c1/wheat/splicer/lib/HasProcessLogger.scala
+++ wheat-splicer/src/main/scala/x7c1/wheat/splicer/lib/HasProcessLogger.scala
@@ -1,8 +1,21 @@
 package x7c1.wheat.splicer.lib
 
-import sbt.ProcessLogger
+import sbt.{Logger, ProcessLogger}
+
+import scala.language.implicitConversions
 
 trait HasProcessLogger {
   def logger: ProcessLogger
 }
+
+object HasProcessLogger {
+  implicit def fromLogger(x: Logger): HasProcessLogger =
+    new HasProcessLogger {
+      override def logger = Logger.log2PLog(x)
+    }
+
+  implicit def fromProcessLogger(x: ProcessLogger): HasProcessLogger =
+    new HasProcessLogger {
+      override def logger = x
+    }
 }
